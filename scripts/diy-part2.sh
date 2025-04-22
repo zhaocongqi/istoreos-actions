35f55b39f993782ae66c1eedf10650cbf1bce4b1
Siriling <97015395+Siriling@users.noreply.github.com>
2025-04-22 22:38:22 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -249,12 +249,12 @@ cp -rf ../../kiddin9/luci-app-unblockneteasemusic/* luci-app-unblockneteasemusic
 # 5G通信模组拨号工具
 mkdir quectel_QMI_WWAN
 mkdir quectel_cm_5G
-mkdir quectel_MHI
-mkdir luci-app-hypermodem
+# mkdir quectel_MHI
+# mkdir luci-app-hypermodem
 cp -rf ../../Modem-Support/quectel_QMI_WWAN/* quectel_QMI_WWAN
 cp -rf ../../Modem-Support/quectel_cm_5G/* quectel_cm_5G
-cp -rf ../../Modem-Support/quectel_MHI/* quectel_MHI
-cp -rf ../../Modem-Support/luci-app-hypermodem/* luci-app-hypermodem
+# cp -rf ../../Modem-Support/quectel_MHI/* quectel_MHI
+# cp -rf ../../Modem-Support/luci-app-hypermodem/* luci-app-hypermodem
 
 # 5G模组短信插件
 # cp -rf temp/luci-app-sms-tool/* luci-app-sms-tool
