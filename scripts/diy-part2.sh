1f00bf45d498e22194db8075e237f185d65cdb3b
zhaocongqi <1229896069@qq.com>
2025-09-20 13:04:06 +0800

    disable openclash
    Signed-off-by: zhaocongqi <1229896069@qq.com>

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -198,14 +198,14 @@ mkdir luci-app-openclash
 cp -rf ../../kiddin9/luci-app-openclash/* luci-app-openclash
 cp -rf ../../MyConfig/configs/istoreos/general/applications/luci-app-openclash/* luci-app-openclash
 #加入OpenClash核心
-chmod -R a+x "$GITHUB_WORKSPACE"/scripts/preset-clash-core.sh
-if [ "$1" = "rk33xx" ]; then
-    "$GITHUB_WORKSPACE"/scripts/preset-clash-core.sh arm64
-elif [ "$1" = "rk35xx" ]; then
-    "$GITHUB_WORKSPACE"/scripts/preset-clash-core.sh arm64
-elif [ "$1" = "x86" ]; then
-    "$GITHUB_WORKSPACE"/scripts/preset-clash-core.sh amd64
-fi
+# chmod -R a+x "$GITHUB_WORKSPACE"/scripts/preset-clash-core.sh
+# if [ "$1" = "rk33xx" ]; then
+#     "$GITHUB_WORKSPACE"/scripts/preset-clash-core.sh arm64
+# elif [ "$1" = "rk35xx" ]; then
+#     "$GITHUB_WORKSPACE"/scripts/preset-clash-core.sh arm64
+# elif [ "$1" = "x86" ]; then
+#     "$GITHUB_WORKSPACE"/scripts/preset-clash-core.sh amd64
+# fi
 
 # 去广告
 #ADGuardHome（kiddin9）
